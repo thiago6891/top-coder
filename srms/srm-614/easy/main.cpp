89fd04457ff15c5f1736f802f3a4e64b3577d842
Thiago <thiago6891@gmail.com>
2014-04-02 13:47:15 -0300

    untested solution

--- srms/srm-614/easy/main.cpp
+++ srms/srm-614/easy/main.cpp
@@ -1,4 +1,20 @@
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
 class MicroStrings {
 public:
 	string makeMicroString(int, int);
+}
+
+string MicroStrings::makeMicroString(int A, int D) {
+	vector<int> nums;
+	int n = A / D;
+	for (int i = 0; i <= n; i++) nums.push_back(A - i * D);
+	
+	stringstream ss;
+	for (int i = 0; i < nums.size(); i++) ss << nums[i];
+	
+	return ss.str();
 }
